580f8a248d7dcf090bdb392a74e3e64023df79ba
Kauan Pires <nauakpires@gmail.com>
2024-02-25 10:07:14 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,69 @@
+const form = document.getElementById('form-atividade');
+let linhas = '';
+let inputNotaArray = []
+let inputNomeArray = []
+let tfoot_td = document.querySelector('td#nota_media')
+let aprovado = document.querySelector('span.resultado')
+const imgAprovado = '<img src="./images/aprovado.png" alt="emoji comemorando">'
+const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionado">'
+const notaMedia = prompt('Digite abaixo a Nota Média:')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    addLinha()
+    console.log(inputNomeArray)
+});
+
+
+
+function addLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+    inputNotaArray.push(Number(inputNotaAtividade.value))
+
+
+
+    for (let c = 0; c < inputNomeArray.length; c++) {
+        if (inputNomeAtividade.value.toUpperCase() == inputNomeArray[c]) {
+            alert('Essa atividade já foi adicionada')
+            return true
+    }
+    } if (inputNomeAtividade.value.toUpperCase() != inputNomeArray) {
+        inputNomeArray.push(inputNomeAtividade.value.toUpperCase())
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaArray[inputNotaArray.length - 1]}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMedia ? imgAprovado : imgReprovado }</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    
+        const corpoTabela = document.querySelector('tbody');
+        corpoTabela.innerHTML = linhas;
+    
+        inputNomeAtividade.focus()
+        inputNomeAtividade.value = '';
+        inputNotaAtividade.value = '';
+        calculaMedia()
+    }
+}
+
+function calculaMedia () {
+    let valortotal = 0
+    for(let pos in inputNotaArray){
+        valortotal += inputNotaArray[pos]
+    }
+    let media = valortotal / inputNotaArray.length
+
+    tfoot_td.innerHTML = ''
+    tfoot_td.innerHTML += (`<b>${media}</b>`)
+    
+    if(media >= notaMedia) {
+        aprovado.innerHTML = 'Aprovado'
+        aprovado.style.background = 'green'
+    } else {
+        aprovado.innerHTML = 'Reprovado'
+        aprovado.style.background = 'red'
+    }
+}
---
+++ test.js
@@ -0,0 +1,9 @@
+let inputNomeArray = ['Kauan', 'Lucas']
+let nome = 'sophia'
+inputNomeArray.push(nome.toUpperCase())
+
+for(c = 0; c < inputNomeArray.length; c++) {
+    console.log(inputNomeArray[c].toUpperCase())
+}
+
+console.log(inputNomeArray)
