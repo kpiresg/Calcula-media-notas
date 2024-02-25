16ff9d05054db1b314f54ddde2ecafd1b6e00f7a
Kauan Pires <nauakpires@gmail.com>
2024-02-25 10:08:14 -0300

    Removendo test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let inputNomeArray = ['Kauan', 'Lucas']
-let nome = 'sophia'
-inputNomeArray.push(nome.toUpperCase())
-
-for(c = 0; c < inputNomeArray.length; c++) {
-    console.log(inputNomeArray[c].toUpperCase())
-}
-
-console.log(inputNomeArray)
